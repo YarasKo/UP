94c1ae4b0e94f5ce71d58cebdeea4e76054c7033
Yaras <yaraslau.kavalenka@gmail.com>
2020-03-23 22:29:23 +0300

    Added tags and likes

--- UI/script.js
+++ UI/script.js
@@ -3,35 +3,45 @@ let posts=[{
     createdAt: new Date('2020-04-17T23:00:00'),
     author: 'Иванов Иван',
     description: 'Post text 1',
-    PhotoLink: 'null'
+    PhotoLink: 'null',
+    tags:['tag0','tag0'],
+    likes:['user1'],
 },
     {
         id:2,
         createdAt: new Date('2020-04-17T23:00:02'),
         author: 'Иванов Иван2',
         description: 'Post text 2',
-        PhotoLink: 'null'
+        PhotoLink: 'null',
+        tags:['tag1','tag2'],
+        likes:['user1'],
     },
     {
         id:3,
         createdAt: new Date('2020-04-17T23:00:03'),
         author: 'Иванов Иван3',
         description: 'Post text 3',
-        PhotoLink: 'null'
+        PhotoLink: 'null',
+        tags:['tag3','tag2'],
+        likes:['user1'],
     },
     {
         id:4,
         createdAt: new Date('2020-03-17T23:00:04'),
         author: 'Иванов Иван4',
         description: 'Post text 4',
-        PhotoLink: 'null'
+        PhotoLink: 'null',
+        tags:['tag4','tag5'],
+        likes:['user1'],
     },
     {
         id:5,
         createdAt: new Date('2020-03-17T23:00:05'),
         author: 'Иванов Иван5',
         description: 'Post text 4',
-        PhotoLink: 'null'
+        PhotoLink: 'null',
+        tags:['tag3','tag4'],
+        likes:['user1'],
     }
     ,
     {
@@ -39,7 +49,9 @@ let posts=[{
         createdAt: new Date('2020-03-17T23:00:06'),
         author: 'Иванов Иван5',
         description: 'Post text 6',
-        PhotoLink: 'null'
+        PhotoLink: 'null',
+        tags:['tag5','tag6'],
+        likes:['user1'],
     }
     ,
     {
@@ -47,7 +59,9 @@ let posts=[{
         createdAt: new Date('2020-03-17T23:00:05'),
         author: 'Иванов Иван5',
         description: 'Post text 4',
-        PhotoLink: 'null'
+        PhotoLink: 'null',
+        tags:['tag7','tag2'],
+        likes:['user1'],
     }
     ,
     {
@@ -55,7 +69,9 @@ let posts=[{
         createdAt: new Date('2020-03-17T23:00:05'),
         author: 'Иванов Иван5',
         description: 'Post text 4',
-        PhotoLink: 'null'
+        PhotoLink: 'null',
+        tags:['tag7','tag1'],
+        likes:['user1'],
     }
     ,
     {
@@ -63,7 +79,9 @@ let posts=[{
         createdAt: new Date('2020-03-17T23:00:05'),
         author: 'Иванов Иван5',
         description: 'Post text 4',
-        PhotoLink: 'null'
+        PhotoLink: 'null',
+        tags:['tag1','tag7'],
+        likes:['user1'],
     }
     ,
     {
@@ -71,7 +89,9 @@ let posts=[{
         createdAt: new Date('2020-03-17T23:00:05'),
         author: 'Иванов Иван5',
         description: 'Post text 4',
-        PhotoLink: 'null'
+        PhotoLink: 'null',
+        tags:['tag1','tag2'],
+        likes:['user1'],
     }
     ,
     {
@@ -79,7 +99,9 @@ let posts=[{
         createdAt: new Date('2020-03-17T23:00:05'),
         author: 'Иванов Иван5',
         description: 'Post text 4',
-        PhotoLink: 'null'
+        PhotoLink: 'null',
+        tags:['tag1','tag2'],
+        likes:['user1'],
     }
     ,
     {
@@ -87,7 +109,9 @@ let posts=[{
         createdAt: new Date('2020-03-17T23:00:05'),
         author: 'Иванов Иван5',
         description: 'Post text 4',
-        PhotoLink: 'null'
+        PhotoLink: 'null',
+        tags:['tag1','tag2'],
+        likes:['user1'],
     }
     ,
     {
@@ -95,7 +119,9 @@ let posts=[{
         createdAt: new Date('2020-03-17T23:00:05'),
         author: 'Иванов Иван5',
         description: 'Post text 4',
-        PhotoLink: 'null'
+        PhotoLink: 'null',
+        tags:['tag1','tag2'],
+        likes:['user1'],
     }
     ,
     {
@@ -103,7 +129,9 @@ let posts=[{
         createdAt: new Date('2020-03-17T23:00:05'),
         author: 'Иванов Иван5',
         description: 'Post text 4',
-        PhotoLink: 'null'
+        PhotoLink: 'null',
+        tags:['tag1','tag2'],
+        likes:['user1'],
     }
     ,
     {
@@ -111,7 +139,9 @@ let posts=[{
         createdAt: new Date('2020-03-17T23:00:05'),
         author: 'Иванов Иван5',
         description: 'Post text 4',
-        PhotoLink: 'null'
+        PhotoLink: 'null',
+        tags:[],
+        likes:[],
     }
     ,
     {
@@ -119,14 +149,18 @@ let posts=[{
         createdAt: new Date('2020-03-17T23:00:05'),
         author: 'Иванов Иван5',
         description: 'Post text 4',
-        PhotoLink: 'null'
+        PhotoLink: 'null',
+        tags:['tag1','tag2'],
+        likes:['user1'],
     },
     {
         id:17,
         createdAt: new Date('2020-03-17T23:00:05'),
         author: 'Иванов Иван5',
         description: 'Post text 4',
-        PhotoLink: 'null'
+        PhotoLink: 'null',
+        tags:['tag1','tag2'],
+        likes:['user1'],
     }
     ,
     {
@@ -134,34 +168,42 @@ let posts=[{
         createdAt: new Date('2020-03-17T23:00:05'),
         author: 'Иванов Иван5',
         description: 'Post text 4',
-        PhotoLink: 'null'
+        PhotoLink: 'null',
+        tags:['tag1','tag2'],
+        likes:['user1'],
     },
     {
         id:19,
         createdAt: new Date('2020-03-17T23:00:05'),
         author: 'Иванов Иван5',
         description: 'Post text 4',
-        PhotoLink: 'null'
+        PhotoLink: 'null',
+        tags:['tag1','tag2'],
+        likes:['user1'],
     },
     {
         id:20,
         createdAt: new Date('2020-03-17T23:00:05'),
         author: 'Иванов Иван5',
         description: 'Post text 4',
-        PhotoLink: 'null'
+        PhotoLink: 'null',
+        tags:['tag1','tag2'],
+        likes:['user1'],
     },
     {
         id:21,
         createdAt: new Date('2020-03-17T23:00:05'),
         author: 'Иванов Иван5',
         description: 'Post text 4',
-        PhotoLink: 'null'
+        PhotoLink: 'null',
+        tags:['tag1','tag2'],
+        likes:['user1'],
     }
     ];
 
 
 //part 2
-(function() {
+;(function() {
 function getPosts(start,top,filt){
     let st=start||0;
     let to=top||10;
@@ -179,28 +221,42 @@ function getPosts(start,top,filt){
 
             })
         }
+        if(typeof filt.tags!='undefined'){
+            newArray=newArray.filter(function (val) {
+                let flag=false;
+                val.tags.forEach(value =>{
+                    if(filt.tags.includes(value)){
+                        flag=true;
+                    }
+                } );
+
+                return flag;
+
+            })
+        }
 
     }
     newArray=newArray.slice(st,st+to);
     newArray.sort(function (post1,post2) {
         return post2.createdAt-post1.createdAt;
-    })
+    });
     return newArray;
 }
 function getPost(id){
     return posts.find(item => item.id === id);
 }
 function validatePost(post) {
-if (typeof post.id!="undefined"&&typeof post.description!="undefined"&&typeof post.author!="undefined"&&typeof post.createdAt!="undefined"){
-    if(post.author.length!=0&& post.description.length<200){
+    let flag=false;
+if (typeof post.id!="undefined"&&typeof post.description!="undefined"&&typeof post.author!="undefined"&&typeof post.createdAt!="undefined"&&typeof post.tags!="undefined"&&typeof post.likes!="undefined"){
+    if(post.author.length!==0&& post.description.length<200){
         let unique = posts.findIndex(item => item.id === post.id);
         if(unique===-1){
-            return true;
+            flag= true;
         }
 
     }
 }
-return false;
+return flag;
 }
 function addPost(post){
     if (validatePost(post)){
@@ -214,6 +270,7 @@ function editPost(id,edit){
     let original={...unchanged};
     original.description=edit.description||original.description;
     original.photoLink=edit.photoLink||original.photoLink;
+    original.tags=edit.tags||original.tags;
     removePost(id);
     if(validatePost(original)){
         addPost(original);
@@ -229,19 +286,22 @@ return posts.splice(posts.findIndex(item => item.id === id),1);
 }
 
 console.log(getPosts(0,10));
-console.log(getPosts((10,10)));
+console.log(getPosts(10,10));
 console.log(getPosts(0,10,{author:"Иванов Иван5"}));
+console.log(getPosts(0,10,{tags:['tag1','tag2']}));
 console.log(getPosts(0,10,{createdAt:new Date('2020-03-17T23:00:00')}));
-console.log(getPost((3)));
+console.log(getPost(3));
 console.log(addPost( {
     id:22,
     createdAt: new Date('2020-03-17T23:00:18'),
     author: 'Иванов Иван22',
     description: 'Post text 22',
+    tags:[],
+    likes:[],
 }));
-    console.log(getPost((22)));
-    console.log(editPost(22,{ description: 'NEW Post text 22',}))
     console.log(getPost(22));
-    console.log(removePost(22))
+    console.log(editPost(22,{ description: 'NEW Post text 22',}));
+    console.log(getPost(22));
+    console.log(removePost(22));
     console.log(getPosts(20,10));
 }());
